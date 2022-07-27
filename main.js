2ae3fdd56e494f46603e4e68dffcd913d69b1565
José Ríos <josemartzrios14@gmail.com>
2022-07-26 21:53:39 -0500

    js done

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-    
 function menu(){
     const hamburguer = document.querySelector(".hamburguer");
     const navMenu = document.querySelector(".navbar-left ul");
@@ -35,5 +34,61 @@ function addToCart(){
 
 }
 
+
+
+function openPopUp(){
+    const btn1 = document.getElementById('open-btn1');
+    const btn2 = document.getElementById('open-btn2');
+    const btn3 = document.getElementById('open-btn3');
+    const btn4 = document.getElementById('open-btn4');
+    const btn5 = document.getElementById('open-btn5');
+
+
+    const modalContainer = document.getElementById('modal_container');
+    const botones = [btn1, btn2, btn3, btn4, btn5];
+    
+    botones.forEach(boton => {
+
+        boton.addEventListener('click', () => {
+            
+            // boton.style.opacity = '0';
+            modalContainer.classList.add('show');
+        });
+
+    });
+
+}
+
+    
+function closePopUp(){
+
+    const btn1 = document.getElementById('close-btn1');
+    const btn2 = document.getElementById('close-btn2');
+    const btn3 = document.getElementById('close-btn3');
+    const btn4 = document.getElementById('close-btn4');
+    const btn5 = document.getElementById('close-btn5');
+
+    const modalContainer = document.getElementById('modal_container');
+    const botones = [btn1, btn2, btn3, btn4, btn5];
+   
+
+    botones.forEach(boton => {
+
+        boton.addEventListener('click', () => {
+            
+            // boton.style.opacity = '0';
+            modalContainer.classList.remove('show');
+        });
+
+    });
+};
+
+
+
+
 addToCart();
 menu();
+openPopUp();
+closePopUp();
+
+
